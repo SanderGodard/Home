fe39e8bade285166ed25c3e9bc8f6aab4093101e
Sander Godard <sander@godard.no>
2018-12-30 20:12:48 +0100

    Worked a lot on the Galleri page

---
+++ Scripts/Search.js
@@ -0,0 +1,15 @@
+function SearchFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("Search");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("SearchList");
+  li = ul.getElementsByTagName("a");
+  for (i = 0; i < li.length; i++) {
+    title = li[i].getElementsByTagName("h3")[0];
+    if (title.innerHTML.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
