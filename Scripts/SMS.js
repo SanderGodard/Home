cf3097eaf653dfddaf5e2c9e0fc2e34025420cda
Sander Godard <sander@godard.no>
2019-03-27 18:32:10 +0100

    Working on some cool SMS button animation

--- Scripts/SMS.js
+++ Scripts/SMS.js
@@ -3,9 +3,22 @@ var current = a.innerHTML;
 
 function SMSfunc() {
   if (a.innerHTML == current) {
-    a.innerHTML = '+47 901 89 461';
+    a.style.width = "0px";
+    //a.style.minWidth = "0px";
+    setTimeout(function() {
+      a.innerHTML = '+47 901 89 461';
+    }, 500);
+    a.style.width = "";
+    //a.style.minWidth = "";
 
   } else {
-    a.innerHTML = current;
+    a.style.width = "0px";
+    //a.style.minWidth = "0px";
+    setTimeout(function() {
+      a.innerHTML = current;
+    }, 500);
+
+    a.style.width = "";
+    //a.style.minWidth = "";
   }
 }
