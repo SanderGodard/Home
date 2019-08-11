e7adf073e505021d325f65cfbc79921108fb4229
Sander Godard <sander@godard.no>
2019-08-11 16:11:18 +0200

    Images, horisontal scroll fix, and some updates and improvements

--- Scripts/Scroll.js
+++ Scripts/Scroll.js
@@ -1,28 +1,35 @@
-//Right Button Scroll
-function Scroll() {
-  var amount = document.getElementById("Mosala").offsetWidth;
-  var div = document.getElementById("Slider");
-  var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-  var v = .8
-  var vw = (w / 100 * v)
-  //div.scrollLeft += amount;
-  div.scrollBy({
-    left: amount + vw,
-    top: 0,
-    behavior: 'smooth'
-  });
+var div = document.getElementById("Slider");
+var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+var prosj = document.getElementsByClassName('Project');
+
+function fixWidth() {
+  for (var i = 0; i < prosj.length; i++) {
+    var sp = prosj[i].lastElementChild.lastElementChild;
+    sp.style.width = (div.offsetWidth - (w / 100));
+  }
 }
-//Left Button Scroll
-function ScrollLeft() {
+
+//Right Button Scroll
+function Scroll(dirr) {
   var amount = document.getElementById("Mosala").offsetWidth;
-  var div = document.getElementById("Slider");
-  var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-  var v = .8
-  var vw = (w / 100 * v)
+  var margin = .5;
+  var vw = (w / 100) + (margin * 2);
   //div.scrollLeft += amount;
-  div.scrollBy({
-    left: -(amount + vw),
-    top: 0,
-    behavior: 'smooth'
-  });
+  if (dirr == "+") {
+
+    div.scrollBy({
+      left: (amount + vw),
+      top: 0,
+      behavior: 'smooth'
+    });
+  } else if (dirr == "-") {
+
+    div.scrollBy({
+      left: -(amount + vw),
+      top: 0,
+      behavior: 'smooth'
+    });
+  } else {
+    console.log("Fiks horisontale scrolling arrows Sander!");
+  }
 }
