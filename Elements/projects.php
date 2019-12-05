9a25c58b6e4f0d731964c569f6378c673d560c10
Sander Godard <sander@godard.no>
2019-12-05 22:56:08 +0100

    Og fikset projects linker

--- Elements/projects.php
+++ Elements/projects.php
@@ -3,59 +3,59 @@
   // MereteGodardFoto
   $Short = "Dette er portfolio nettsiden til moren min, Merete, hvor hun viser frem bilder hun tar.";
   $Long = "Dette er portfolio nettsiden til moren min, Merete, hvor hun viser frem bilder hun tar. Det vanskeligste med denne nettsiden er det kreative designet, og hvordan selve collagen inne på galleriet er laget. Dette er et lite prosjekt som er skrevet i bare HTML, CSS og JS.";
-  $MereteGodardFoto = ["name" => "Merete Godard Foto", "link" => "/MereteGodardFoto/", "short" => $Short, "long" => $Long];
+  $MereteGodardFoto = ["name" => "Merete Godard Foto", "link" => "http://sandergodard.com/MereteGodardFoto/", "short" => $Short, "long" => $Long];
 
   // Mosala
   $Short = "En nettservice hvor startup bedrifter lett kan møte folk som er interresserte i å jobbe";
   $Long = "En nettservice hvor startup bedrifter lett kan møte folk som er interresserte i å jobbe<br><br>
   I denne jobben hadde arbeidsgiveren allerede en idé om hvordan han ville nettsiden skulle se ut, så hoved-layoutet på et par av sidene
   var allerede gjort. Ellers designet jeg alt, både UI og UX messig, og kodet nettsidene i PHP og Sass.";
-  $Mosala = ["name" => "Mosala", "link" => "/Mosala/", "short" => $Short, "long" => $Long];
+  $Mosala = ["name" => "Mosala", "link" => "http://sandergodard.com/Mosala/", "short" => $Short, "long" => $Long];
 
   // Skole side
   $Short = "Denne nettsiden har jeg til skolearbeid, her legger jeg ut arbeidsoppgaver og prosjekter.";
   $Long = "Her er alt fra arbeidsoppgaver til skoleprosjekter i IT1 faget.
   Dette er den første nettsiden jeg har laget, og jeg holder ofte på med å legge til/ endre ting på siden.
   Her kan du også lese mer om hva jeg jobber meg, og hvilke verktøy jeg bruker. Siden har jeg skrevet i vanlig CSS og PHP.";
-  $Skole = ["name" => "Skole Side", "link" => "/www.klasserom/", "short" => $Short, "long" => $Long];
+  $Skole = ["name" => "Skole Side", "link" => "http://sandergodard.com/www.klasserom/", "short" => $Short, "long" => $Long];
 
   // NewYork
   $Short = "Utforsk New York, her kan du se en ungdomsklubb sin tur til byen. Dette var også et skoleprosjekt.";
   $Long = "Utforsk New York, her kan du se en ungdomsklubb sin tur til byen.<br><br>
   Dette er et skole-samarbeidsprosjekt som jeg lagde ganske tidlig. Siden sitt formål er å fortelle om denne ungdomsklubbens planlagte tur til New York. Dette prosjektet lærte meg veldig mye om CSS og HTML generelt, og jeg brukte bare de to
   språkene mens jeg jobbet på denne siden, pluss bittelitt PHP.";
-  $NewYork = ["name" => "New York", "link" => "/www.klasserom/Prosjekter/Ungdomsklubb/", "short" => $Short, "long" => $Long];
+  $NewYork = ["name" => "New York", "link" => "http://sandergodard.com/www.klasserom/Prosjekter/Ungdomsklubb/", "short" => $Short, "long" => $Long];
 
   // HypeIT
   $Short = "Et skoleprosjekt hvor vi lager nettbutikk, jeg var leder for front-end avdelingen i prosjektet.";
   $Long = "Her samarbeider hele klassa om å lage en fake nettbutikk. Jeg er lederen for front-end avdelingen, og dette prosjektet krevde ganske mye. Jeg samarbeidet med flere andre som drev med front-end jobben, og vi måtte organisere og jobbe mye
   med Sass, PHP, HTML og JS.";
-  $HypeIT = ["name" => "HypeIT", "link" => "/HypeIT/", "short" => $Short, "long" => $Long];
+  $HypeIT = ["name" => "HypeIT", "link" => "http://sandergodard.com/HypeIT/", "short" => $Short, "long" => $Long];
 
   // T-bane
   $Short = "Et skoleprosjekt hvor vi skulle lage en nettside som var koblet til en database. ";
   $Long = "Her var oppgaven å lage en database med datamodell, så koble databasen til en nettside, og bruke databasen til å hente informasjonen vi trengte til nettsiden. Dette prosjektet krevde mye mer avansert PHP og litt SQL, samt selvfølgelig
   Sass, litt JS, og HTML.";
-  $Tbane = ["name" => "T-bane", "link" => "/www.klasserom/prosjekter/T-banestasjoner/", "short" => $Short, "long" => $Long];
+  $Tbane = ["name" => "T-bane", "link" => "http://sandergodard.com/www.klasserom/prosjekter/T-banestasjoner/", "short" => $Short, "long" => $Long];
 
   // nLose
   $Short = "En veldig liten og rask nettside om et produkt vi måtte finne på selv i skolen.";
   $Long = "En veldig liten og rask nettside om et produkt vi måtte finne på selv i skolen. Nettsiden er ekstremt lett-laget, og ikke veldig pen. Men det er blandt de første jeg lagde, og med denne nettsiden lærte jeg mye på kort tid om web-design og
   brukeropplevelser. På denne siden brukte jeg bare HTML og CSS.";
-  $nLose = ["name" => "nLose", "link" => "/www.klasserom/Prosjekter/nLose/", "short" => $Short, "long" => $Long];
+  $nLose = ["name" => "nLose", "link" => "http://sandergodard.com/www.klasserom/Prosjekter/nLose/", "short" => $Short, "long" => $Long];
 
   // Digitale Medier
   $Short = "Enda en mindre og raskere nettside jeg lagde for å vise frem en reklamevideo i skolen.";
   $Long = "Enda en mindre og raskere nettside jeg lagde for å vise frem en reklamevideo i skolen. Siden ble laget på under 60 minutter, og ble som sagt bare laget for å ha en side å presentere en reklamevideo for en liksom-bedrift som et
   gruppeprosjekt.
   På denne siden brukte jeg bare HTML og CSS.";
-  $DigitaleMedier = ["name" => "Digitale Medier", "link" => "/www.klasserom/Eget%20arbeid/Digitale%20medier/", "short" => $Short, "long" => $Long];
+  $DigitaleMedier = ["name" => "Digitale Medier", "link" => "http://sandergodard.com/www.klasserom/Eget%20arbeid/Digitale%20medier/", "short" => $Short, "long" => $Long];
 
   // RNG
   $Short = "RNG Classroom er et lite verktøy for læreren så de kan lettere engasjere elevene i timen.";
   $Long = "RNG Classroom er et lite verktøy for læreren så de kan lettere engasjere elevene i timen, den kan velge ut tilfeldig elev til å svare på spørsmålet, fikse tilfeldig plassering, tar hensyn til fravær og mer.
   Den er laget for å fungere spesielt med klassen min sitt oppsett, og det er to lister i filene som må endres for at den skal kunne brukes av andre klasser.";
-  $RNG = ["name" => "RNG Classroom", "link" => "/RNG/", "short" => $Short, "long" => $Long];
+  $RNG = ["name" => "RNG Classroom", "link" => "http://sandergodard.com/RNG/", "short" => $Short, "long" => $Long];
 
 
   $projects = array($MereteGodardFoto, $Mosala, $Skole, $NewYork, $HypeIT, $Tbane, $nLose, $DigitaleMedier, $RNG);
