f6cc7557d1f95ea17f9e144dcd6b50a442b9b873
Sander Godard <sander@godard.no>
2019-12-23 18:46:05 +0100

    Fikset desktop scrolling horisontalt
    Bug hvor den går inn i project etterpå, men er greit nok.

--- Scripts/Scroll.js
+++ Scripts/Scroll.js
@@ -33,3 +33,38 @@ function Scroll(dirr) {
     console.log("Fiks horisontale scrolling arrows Sander!");
   }
 }
+
+// copy past
+let isDown = false;
+let startX;
+let scrollLeft;
+
+div.addEventListener('mousedown', (e) => {
+  isDown = true;
+  div.classList.add('drar');
+  e.preventDefault();
+  startX = e.pageX - div.offsetLeft;
+  scrollLeft = div.scrollLeft;
+});
+div.addEventListener('mouseleave', (e) => {
+  isDown = false;
+  e.preventDefault();
+  div.classList.remove('drar');
+});
+div.addEventListener('mouseup', (e) => {
+  isDown = false;
+  e.preventDefault();
+  div.classList.remove('drar');
+});
+div.addEventListener('mousemove', (e) => {
+  if(!isDown) return;
+  e.preventDefault();
+  // pr = e.children;
+  // for (var i = 0; i < pr.length; i++) {
+  //   pr[i].preventDefault();
+  // }
+  const x = e.pageX - div.offsetLeft;
+  const walk = (x - startX) * 1; //scroll-fast
+  div.scrollLeft = scrollLeft - walk;
+  //console.log(walk);
+});
